f5174afca6623ee4093dd141b0b54505f97b7814
saioufe <saioufeyoutube@yahoo.com>
2022-08-17 14:45:38 -0500

    toolbar with title and start of the slider

--- app/src/main/java/com/example/screendesign2/MainActivity.java
+++ app/src/main/java/com/example/screendesign2/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.screendesign2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -10,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Toolbar toolbar = findViewById(R.id.main_toolbar);
+        toolbar.setTitle("");
+        toolbar.setBackgroundColor(getResources().getColor(R.color.white , getResources().newTheme()));
+        setSupportActionBar(toolbar);
     }
 }
